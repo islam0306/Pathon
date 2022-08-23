6c16b0d2f3ae645dfd06da3624d127d093496af5
islam0306 <i_shamilov@sotex.ru>
2022-08-23 13:32:13 +0300

    Task

---
+++ Lesson-3/Task-1.py
@@ -0,0 +1,26 @@
+'''
+Реализовать функцию, принимающую два числа
+(позиционные аргументы) и выполняющую их деление.
+Числа запрашивать у пользователя,
+предусмотреть обработку ситуации деления на ноль.
+'''
+
+
+def div(*args):
+    try:
+        arg1 = int(input("Input dividend "))
+        arg2 = int(input("Input divider "))
+        res = arg1 / arg2
+    except ValueError:
+        return 'Value error'
+    except ZeroDivisionError:
+        return "Wrong devider! You can't use zero as a devider"
+
+    return res
+
+
+
+
+
+
+print(f'result  {div()}')
